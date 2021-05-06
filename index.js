0f1c95ba841367ad4ed452e711b6ed0fb7985d0c
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2021-05-06 18:21:32 -0300

    teste

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/url", (req, res, next) => {
   res.json(["Teste"]);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
  console.log("Server running on port 3000");
 });
