03a0c929f81ccb7e5ab0c82e04e1ef92a95f8b5d
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2021-05-06 19:19:14 -0300

    agora bai

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require("express");
+var bodyParser = require('body-parser')
+
 var app = express();
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.post('/', (req, res) => {
-  const { pessoas, quartos, tipo} = req.query;
+  const { pessoas, quartos, tipo} = req.body;
 
   const resultado = configQuartos.filter(
     quarto => quarto.pessoas == pessoas &&
